dad38bd030cfd6028983daf21e188729a2297e16
adrianyyxy <58200990+adrianyyxy@users.noreply.github.com>
2021-10-10 17:12:33 -0500

    Else if

--- Keyboard.cydsn/main.c
+++ Keyboard.cydsn/main.c
@@ -25,13 +25,13 @@ CY_ISR(INT_IT){
     if(c==1){
         Columns_Write(0x0E);
     }
-    if(c==2){
+    else if(c==2){
         Columns_Write(0x0D);
     }
-    if(c==3){
+    else if(c==3){
         Columns_Write(0x0B);
     }
-    if(c==4){
+    else if(c==4){
         Columns_Write(0x07);
         c=0;
     }
@@ -59,19 +59,19 @@ CY_ISR(INT_KB){
         LCD_PutChar('1');
         p++;
     }
-    if(key==0x7D){
+    else if(key==0x7D){
         LCD_Position(1,p);
         str[p]='2';
         LCD_PutChar('2');
         p++;
     }
-    if(key==0x7B){
+    else if(key==0x7B){
         LCD_Position(1,p);
         str[p]='3';
         LCD_PutChar('3');
         p++;
     }
-    if(key==0x77){  //A - Delete the previus position
+    else if(key==0x77){  //A - Delete the previus position
         //////////////////////
         //the if function is needed so as not to break the function with negative numbers / positions 
         if(0<p){    //as long as p is greater than 0, we can return to the previus position
@@ -86,73 +86,73 @@ CY_ISR(INT_KB){
         }
         //////////////////////
     }
-    if(key==0xBE){
+    else if(key==0xBE){
         LCD_Position(1,p);
         str[p]='4';
         LCD_PutChar('4');
         p++;
     }
-    if(key==0xBD){
+    else if(key==0xBD){
         LCD_Position(1,p);
         str[p]='5';
         LCD_PutChar('5');
         p++;
     }
-    if(key==0xBB){
+    else if(key==0xBB){
         LCD_Position(1,p);
         str[p]='6';
         LCD_PutChar('6');
         p++;
     }
-    if(key==0xB7){  //B - Delete all
+    else if(key==0xB7){  //B - Delete all
         LCD_ClearDisplay(); //Clear the display
         message();  //Print a message
         strcpy(str,""); //Clear the chain variable
         p=0;    //Restart the position of the chain and the prints functions
     }
-    if(key==0xDE){
+    else if(key==0xDE){
         LCD_Position(1,p);
         str[p]='7';
         LCD_PutChar('7');
         p++;
     }
-    if(key==0xDD){
+    else if(key==0xDD){
         LCD_Position(1,p);
         str[p]='8';
         LCD_PutChar('8');
         p++;
     }
-    if(key==0xDB){
+    else if(key==0xDB){
         LCD_Position(1,p);
         str[p]='9';
         LCD_PutChar('9');
         p++;
     }
-    if(key==0xD7){  //C - No fuction
+    else if(key==0xD7){  //C - No fuction
         LCD_Position(1,p);
         str[p]='1';
         LCD_PutChar('1');
         p++;
     }
-    if(key==0xEE){  //* - No fuction
+    else if(key==0xEE){  //* - No fuction
         LCD_Position(1,p);
         str[p]='1';
         LCD_PutChar('1');
         p++;
     }
-    if(key==0xED){
+    else if(key==0xED){
         LCD_Position(1,p);
         str[p]='0';
         LCD_PutChar('0');
         p++;
     }
-    if(key==0xEB){  //# - No fuction
+    else if(key==0xEB){  //# - No fuction
         LCD_Position(1,p);
         str[p]='1';
         LCD_PutChar('1');
         p++;
     }
-    if(key==0xE7){  //D - Enter or change to the window (?) its something like that
+    else if(key==0xE7){  //D - Enter or change to the window (?) its something like that
         LCD_ClearDisplay(); //clear the screen
         LCD_Position(0,0);  //select the position
         LCD_PrintString(str);   //write the chain
